9346118215f674708f74b5b1a49a3296b55c51ed
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-07-01 10:53:25 -0400

    add feature high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,18 +151,26 @@ var startGame = function() {
     } 
     // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
     endGame();
-};
-
+}; // end of start game function
 
 // function to end the entire game 
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highScore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
     // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        window.alert(playerInfo.name + " now has a high score of " + playerInfo.money + "!");
     } else {
-        window.alert("You've lost your robot in battle.");
+        window.alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
